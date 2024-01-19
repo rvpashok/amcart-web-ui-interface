42d241b8fdbb5f7eaa6881a56c8a50951bbd2b43
rvpashok <rvpashokkumar@gmail.com>
2024-01-20 00:13:53 +0530

    APIGatwway endpoint configured

--- src/app/Service/product-service.ts
+++ src/app/Service/product-service.ts
@@ -11,7 +11,8 @@ export class ProductService{
     constructor(private http: HttpClient) { }
 
     fetchSearchCategoryData(){
-        return this.http.get<Array<CategorySearchResponse>>("http://localhost:9010/orchestrationservices/api/search/categories",{"responseType":"json"})
+       // return this.http.get<Array<CategorySearchResponse>>("http://localhost:9010/orchestrationservices/api/search/categories",{"responseType":"json"})
+        return this.http.get<Array<CategorySearchResponse>>("https://ojx3smmf5b.execute-api.ap-south-1.amazonaws.com/orchestrationservices/api/search/categories",{"responseType":"json"})
         .pipe(
             map(res => {
                 console.log('Pipe reponse' + res);
@@ -21,7 +22,8 @@ export class ProductService{
         
 }
     fetchProductDetails(productId : string){
-        return this.http.get<ProductDetailResponse>("http://localhost:9010/orchestrationservices/api/products/" + productId,{"responseType":"json"})
+       //return this.http.get<ProductDetailResponse>("http://localhost:9010/orchestrationservices/api/products/" + productId,{"responseType":"json"})
+        return this.http.get<ProductDetailResponse>("https://ojx3smmf5b.execute-api.ap-south-1.amazonaws.com/orchestrationservices/api/products/" + productId,{"responseType":"json"})
         .pipe(
             map(res => {
                 console.log('Pipe reponse' + res);
