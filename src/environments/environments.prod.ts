73a5a82ae256799a263d80467ba8320f33342a9f
rvpashok <rvpashokkumar@gmail.com>
2024-03-03 19:37:35 +0530

    Env file added

---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,4 @@
+import { Environment } from "../app/model/common-models"
+export const environment: Environment = {
+    baseUrl: "https://ojx3smmf5b.execute-api.ap-south-1.amazonaws.com/"
+}
---
+++ src/environments/environments.ts
@@ -0,0 +1,4 @@
+import { Environment } from "../app/model/common-models"
+export const environment: Environment = {
+    baseUrl: "http://localhost:9010/"
+}
